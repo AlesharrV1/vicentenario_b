59dd7ad5b96329f694cc66ec56f1663939003a55
luisgerman <27436197+AlesharrDev@users.noreply.github.com>
2025-04-25 11:05:47 -0400

    Define Historia entity with properties and TypeORM decorators

--- src/historias/entities/historia.entity.ts
+++ src/historias/entities/historia.entity.ts
@@ -1,1 +1,11 @@
-export class Historia {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'historias' })
+export class Historia {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ type: 'text' })
+  titulo: string;
+  @Column({ type: 'smallint', width: 1, default: 1 })
+  estado: number;
+}
